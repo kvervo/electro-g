83160c266abdd7332f910e3f20a05b40e6f19944
Supay <37410+kvervo@users.noreply.github.com>
2018-09-14 13:47:32 +0300

    Fix module reference

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
-const { app, BrowserWindow, Menu, shell, desktopCapturer } = electron;
-const { template } = require('./menuTemplatemenuTemplate');
+const { app, BrowserWindow, Menu, shell } = electron;
+const { template } = require('./menuTemplate');
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
